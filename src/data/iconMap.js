1c3c2d1e19ec67ebfbc217a0f2c489093abbe70a
cerebralentropy <cerebralentropy@gmail.com>
2026-03-11 15:51:05 -0600

    Changing the icon for flo to popcorn

--- src/data/iconMap.js
+++ src/data/iconMap.js
@@ -1,10 +1,10 @@
-import { Layers2, RectangleVertical, HandPlatter, RectangleHorizontal } from "lucide-react";
+import { Layers2, RectangleVertical, RectangleHorizontal, Popcorn } from "lucide-react";
 import Hive from "../assets/hive.svg?react"
 
 export const iconMap = {
     layers2: Layers2,
     rectangleVertical: RectangleVertical,
-    handPlatter: HandPlatter,
+    popcorn: Popcorn,
     rectangleHorizontal: RectangleHorizontal,
     hive: Hive,
 }
